f9cc793b2afc452405f6f9c9d8beb50a61601779
Peter, Cheung <2220354896@qq.com>
2023-12-03 21:59:29 +0800

    gen board done

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 from structs import *
-from random import choice, choices, shuffle, randint
+from random import choice, shuffle, randint
 from colorama import Fore, Style
 
 class Board:
@@ -12,11 +12,14 @@ class Board:
         self.node_ls:list[Node] = []
     
     def get(self, pos:Position) -> Box:
+        """Get box in `pos`"""
         # if out of bound
         if pos.row < 0 or pos.row >= self.height or pos.col < 0 or pos.col >= self.width:
                return None
         return self.board[pos.row][pos.col]
+    
     def get_relative(self, box:Box, dir:Direction) -> Box:
+        """Get the nearest box in `dir` from `box`"""
         return self.get(box.position.add(dir))
 
     def print_board(self):
@@ -32,14 +35,48 @@ class Board:
         print(f'{Fore.CYAN}{"#"*(self.width*4+1)}{Style.RESET_ALL}')
 
         # body
-        
+        for r_idx, row in enumerate(self.board):
+            print(f'{Fore.GREEN}{"0" if r_idx < 10 else ""}{r_idx}{Fore.CYAN}#', end='')
+            print(Style.RESET_ALL, end='')
+            for c_idx, box in enumerate(row):
+                padding_col = '  '
+                if c_idx < self.width-1:
+                    if (box.is_node() and box.node_right is not None):
+                        padding_col = '--' if box.right_line_cnt == 1 else '=='
+                    if (box.is_line() and box.dir == Direction.RIGHT()):
+                        padding_col = '==' if box.is_double else '--'
+                print(box, end=padding_col)
+            print()
+            
+            # padding row
+            if r_idx == self.height-1:
+                continue
+            print(f'{Fore.CYAN}  #{Style.RESET_ALL}', end='')
+            for c_idx, box in enumerate(row):
+                col = '  '
+                if (box.is_node() and box.node_bottom is not None):
+                    col = ' |' if box.bottom_line_cnt == 1 else '||'
+                if (box.is_line() and box.dir == Direction.UP()):
+                    col = '||' if box.is_double else ' |'
+                print(col, end='  ')
+            print()
+            
 
-        def generate(self, _n:int):
+    def generate(self, _n:int):
         """
             Generate a new game board
             Args:
                 `n`: number of node in the board
         """
+        def _find_empty_boxes(__node:Node, __dir:Direction) -> list[Position]:
+            """Find all empty boxed in a direction from a node"""
+            __res = []
+            __box = self.get_relative(__node, __dir)
+            while __box is not None and __box.is_empty():
+                __res.append(__box.position)
+                __box = self.get_relative(__box, __dir)
+            return __res
+
         # first constrcut an answer board
         self.board = [
             [Box(row, col) for col in range(self.width)] for row in range(self.height)
@@ -48,15 +85,6 @@ class Board:
         # choices n boxes as node
         _avai_node_set : set[Node] = set()
         _dir_ls = [Direction.UP(), Direction.BOTTOM(), Direction.LEFT(), Direction.RIGHT()]
-
-        def _find_empty_boxes(__node:Node, __dir:Direction) -> list[Position]:
-            __res = []
-            __box = self.get_relative(__node, __dir.add(__dir))
-            while __box is not None and __box.is_empty():
-                __res.append(__box.position)
-                __box = self.get_relative(__box, __dir)
-            return __res
-
         for _i in range(_n):
             if _i == 0:
                 # first node, randomly choice a box
@@ -71,15 +99,18 @@ class Board:
             else:
                 # after first node
                 _pos = None
+                _dir = None
 
+                # find an availabe node to connect
                 while _pos is None:
                     _from_node = choice(list(_avai_node_set))
                     shuffle(_dir_ls)
 
-                    for _dir in _dir_ls:
-                        _empty_box_pos_ls = _find_empty_boxes(_from_node, _dir)
+                    for _d in _dir_ls:
+                        _empty_box_pos_ls = _find_empty_boxes(_from_node, _d)
                         if len(_empty_box_pos_ls) != 0:
                             _pos = choice(_empty_box_pos_ls)
+                            _dir = _d
                             break
 
                     # if pos is None in here, from_node cannot connect more node
@@ -101,7 +132,45 @@ class Board:
                 # increase line counter of both node
                 _from_node.n += 1
                 _to_node.n += 1
+                
+                # set linked node
+                _from_node.link_node(_to_node, _dir)
+                _to_node.link_node(_from_node, _dir.opposite())
 
+        # for each connection, 50% chance to make it double
+        for _node in self.node_ls:
+            if _node.node_right is not None and randint(0, 1) == 1:
+                # draw a line
+                self.draw_line(_node, _node.node_right)
+
+                # increase line counter of both node
+                _node.n += 1
+                _node.node_right.n += 1
+                
+                # set linked node
+                _node.link_node(_node.node_right, Direction.RIGHT())
+                _node.node_right.link_node(_node, Direction.LEFT())
+                
+            if _node.node_bottom is not None and randint(0, 1) == 1:
+                # draw a line
+                self.draw_line(_node, _node.node_bottom)
+
+                # increase line counter of both node
+                _node.n += 1
+                _node.node_bottom.n += 1
+                
+                # set linked node
+                _node.link_node(_node.node_bottom, Direction.BOTTOM())
+                _node.node_bottom.link_node(_node, Direction.UP())
+                
+        # clear all link
+        for r_idx, row in enumerate(self.board):
+            for c_idx, box in enumerate(row):
+                if box.is_node():
+                    box.clear_link()
+                elif box.is_line():
+                    self.board[r_idx][c_idx] = box.to_Box()
+        
     def draw_line(self, from_node:Node, to_node:Node):
         horizon = from_node.position.row == to_node.position.row 
         vertical = from_node.position.col == to_node.position.col
@@ -111,12 +180,19 @@ class Board:
         box = self.get_relative(from_node, dir)
         while box.position != to_node.position:
             if horizon:
-                _line = box.to_hl()
+                if box.is_line():
+                    box.is_double = True
+                else:
+                    _line = box.to_hl()
+                    self.board[box.position.row][box.position.col] = _line
             else:
-                _line = box.to_vl()
-            self.board[box.position.row][box.position.col] = _line
+                if box.is_line():
+                    box.is_double = True
+                else:
+                    _line = box.to_vl()
+                    self.board[box.position.row][box.position.col] = _line
             box = self.get_relative(box, dir)
 
-board = Board(18, 8)
-board.generate(10)
+board = Board(15, 10)
+board.generate(46)
 board.print_board()
--- structs.py
+++ structs.py
@@ -25,6 +25,9 @@ class Direction():
 
     def add(self, _dir):
         return Direction(self.v+_dir.v, self.h+_dir.h)
+    
+    def opposite(self) -> Direction:
+        return Direction(-self.v, -self.h)
 
 class Position:
     def __init__(self, row, col):
@@ -44,7 +47,7 @@ class Position:
         _v = int(self.row / abs(self.row)) if self.row != 0 else 0
         _h = int(self.col / abs(self.col)) if self.col != 0 else 0
         return Direction(_v, _h)
-
+    
 # an empty box in the game board
 class Box:
     def __init__(self, row, col):
@@ -58,6 +61,12 @@ class Box:
 
     def is_line(self):
         return False
+    
+    def is_node(self):
+        return False
+        
+    def to_Box(self) -> Box:
+        return Box(self.position.row, self.position.col)
         
     def to_Node(self) -> Node:
         return Node(self.position.row, self.position.col)
@@ -78,20 +87,72 @@ class Box:
 class Node(Box):
     def __init__(self, row, col):
         super().__init__(row, col)
-        self.n = 0
+        self.n : int = 0
+        
         self.node_up : Node = None
         self.node_bottom : Node = None
-        self.node_left : Noe = None
+        self.node_left : Node = None
         self.node_right : Node = None
+        
+        self.up_line_cnt : int = 0
+        self.bottom_line_cnt : int = 0
+        self.left_line_cnt : int = 0
+        self.right_line_cnt : int = 0
 
     def __str__(self):
-        return f"0{self.n}"
+        return f"${self.n}"
 
     def is_empty(self):
         return False
-
-    def is_line(self):
-        return False
+    
+    def is_node(self):
+        return True
+    
+    def link_node(self, _node:Node, _dir:Direction):
+        if _dir == Direction.UP():
+            self.node_up = _node
+            self.up_line_cnt += 1
+        elif _dir == Direction.BOTTOM():
+            self.node_bottom = _node
+            self.bottom_line_cnt += 1
+        elif _dir == Direction.LEFT():
+            self.node_left = _node
+            self.left_line_cnt += 1
+        elif _dir == Direction.RIGHT():
+            self.node_right = _node
+            self.right_line_cnt += 1
+            
+    def unlink_node(self, _dir:Direction):
+        if _dir == Direction.UP():
+            self.node_up.bottom_line_cnt = 0
+            self.up_line_cnt = 0
+            self.node_up.node_bottom = None
+            self.node_up = None
+        elif _dir == Direction.BOTTOM():
+            self.node_bottom.up_line_cnt = 0
+            self.bottom_line_cnt = 0
+            self.node_bottom.node_up = None
+            self.node_bottom = None
+        elif _dir == Direction.LEFT():
+            self.node_left.right_line_cnt = 0
+            self.left_line_cnt = 0
+            self.node_left.node_right = None
+            self.node_left = None
+        elif _dir == Direction.RIGHT():
+            self.node_right.left_line_cnt = 0
+            self.right_line_cnt = 0
+            self.node_right.node_left = None
+            self.node_right = None
+            
+    def clear_link(self):
+        if self.node_up is not None:
+            self.unlink_node(Direction.UP())
+        if self.node_bottom is not None:
+            self.unlink_node(Direction.BOTTOM())
+        if self.node_left is not None:
+            self.unlink_node(Direction.LEFT())
+        if self.node_right is not None:
+            self.unlink_node(Direction.RIGHT())
         
 # Line 
 class Line(Box):
@@ -108,13 +169,13 @@ class Line(Box):
 
 class VerticalLine(Line):
     def __init__(self, row, col):
-        super().__init__(row, col, Direction.UP)
+        super().__init__(row, col, Direction.UP())
     def __str__(self):
         return '||' if self.is_double else ' |'
 
 class HorizonLine(Line):
     def __init__(self, row, col):
-        super().__init__(row, col, Direction.RIGHT)
+        super().__init__(row, col, Direction.RIGHT())
     def __str__(self):
         return '==' if self.is_double else '--'
 
